935210cb675f9b34cb89c605c600a5dcb8af8564
Andrea Sella <andrea.sella@radicalbit.io>
2016-03-01 11:39:33 +0100

    added docs and requirements, moved ActorHandler as AkkaSink's internal class

--- flink-streaming-connectors/flink-connector-akka/src/main/scala/org/apache/flink/connectors/akka/streaming/AkkaSink.scala
+++ flink-streaming-connectors/flink-connector-akka/src/main/scala/org/apache/flink/connectors/akka/streaming/AkkaSink.scala
@@ -27,46 +27,82 @@ import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
 import scala.concurrent.Await
 import scala.concurrent.duration.FiniteDuration
 
-class AkkaSink[IN](sinkName: String, path: String, config: Config*) extends RichSinkFunction[IN] {
+/**
+  * Sink that emits its input elements to a remote actor
+  *
+  * @param systemName Actor System's name, default is "akka-sink"
+  * @param path remote actor's path
+  * @param config Sequence of Akka System configuration
+  * @tparam IN Type of the elements emitted by this sink
+  */
+class AkkaSink[IN](systemName: String = "akka-sink", path: String, config: Seq[Config]) extends RichSinkFunction[IN] {
 
   @transient private var actorSystem: ActorSystem = _
 
   @transient private var handler: ActorRef = _
 
+  /**
+    * Starts underlying Actor System and ActorHandler to manage connection from Sink to remote Actor
+    *
+    * @param parameters The configuration containing the parameters attached to the contract.
+    * @throws IllegalArgumentException number of configurations must be equal to function's parallelism
+    *                                  remote Actor not found via path
+    */
   @throws[IllegalStateException]
   override def open(parameters: Configuration): Unit = {
-    val index = getRuntimeContext.getIndexOfThisSubtask
-    val systemName = s"$sinkName-$index"
 
+    require(
+      config.size == getRuntimeContext.getNumberOfParallelSubtasks,
+      "config.size must be equal to operator's parallelism"
+    )
+
+    val index = getRuntimeContext.getIndexOfThisSubtask
+    val systemName = s"$systemName-$index"
+
+    // hardcoded
     val timeout = new FiniteDuration(10, TimeUnit.SECONDS)
+
     actorSystem = ActorSystem(systemName, config(index))
 
+    // check remote actor exists
     val remote = try {
       Await.result(actorSystem.actorSelection(path).resolveOne(timeout), timeout)
     } catch {
-      case e: Throwable => throw new IllegalStateException(s"Actor not found: $path", e)
+      case e: Throwable => throw new IllegalArgumentException(s"Actor not found: $path", e)
     }
-     handler = actorSystem.actorOf(Props(new ActorHandler(remote)))
+
+    handler = actorSystem.actorOf(Props(new ActorHandler(remote)))
+
   }
 
   override def invoke(value: IN): Unit = {
     handler ! value
   }
 
+  /**
+    * Stops ActorHandler and shutdowns the Actor System
+    */
   override def close(): Unit = {
+    actorSystem.stop(handler)
     actorSystem.shutdown()
   }
-}
 
-final class ActorHandler(remote: ActorRef) extends Actor {
+  /**
+    * Sends values to remote actor and watch it in case of unexpected termination
+    *
+    * @param remote ActorRef's remote actor
+    */
+  final class ActorHandler(remote: ActorRef) extends Actor {
 
-  context.watch(remote)
-  override def receive: Receive = {
-    case Terminated(actorRef) if remote == actorRef =>
-      throw new IOException(s"Actor ${remote.path} has been terminated")
-      context.stop(self)
-    case Terminated(actorRef) if self == actorRef =>
-      context.unwatch(remote)
-    case element => remote ! element
+    context.watch(remote)
+
+    override def receive: Receive = {
+      case Terminated(actorRef) if remote == actorRef =>
+        throw new IOException(s"Actor ${remote.path} has been terminated")
+        context.stop(self)
+      case Terminated(actorRef) if self == actorRef =>
+        context.unwatch(remote)
+      case element => remote ! element
+    }
   }
 }
---
+++ flink-streaming-connectors/flink-connector-akka/src/test/scala/org/apache/flink/connectors/akka/streaming/AkkaSinkITCase.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.connectors.akka.streaming
+
+/**
+  * Created by andreasella on 01/03/16.
+  */
+class AkkaSinkITCase extends {
+  
+}

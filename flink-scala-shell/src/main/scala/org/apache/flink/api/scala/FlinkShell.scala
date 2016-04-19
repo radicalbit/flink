47daf03bb4021fb6161c16ba4ed8ade69659f77a
Till Rohrmann <trohrmann@apache.org>
2016-04-19 15:04:37 +0200

    [FLINK-3775] [shell] Load Flink configuration before forwarding it
    This commit makes sure that the GlobalConfiguration is loaded before the FlinkShell
    is started.

--- flink-scala-shell/src/main/scala/org/apache/flink/api/scala/FlinkShell.scala
+++ flink-scala-shell/src/main/scala/org/apache/flink/api/scala/FlinkShell.scala
@@ -167,6 +167,11 @@ object FlinkShell {
   def startShell(config: Config): Unit = {
     println("Starting Flink Shell:")
 
+    // load global configuration
+    val confDirPath = CliFrontend.getConfigurationDirectoryFromEnv
+    val configDirectory = new File(confDirPath)
+    GlobalConfiguration.loadConfiguration(configDirectory.getAbsolutePath)
+
     val (repl, cluster) = try {
       val (host, port, cluster) = fetchConnectionInfo(config)
       val conf = cluster match {
@@ -216,14 +221,12 @@ object FlinkShell {
     val jarPath = new Path("file://" +
       s"${yarnClient.getClass.getProtectionDomain.getCodeSource.getLocation.getPath}")
     yarnClient.setLocalJarPath(jarPath)
-
-    // load configuration
+    
     val confDirPath = CliFrontend.getConfigurationDirectoryFromEnv
     val flinkConfiguration = GlobalConfiguration.getConfiguration
     val confFile = new File(confDirPath + File.separator + "flink-conf.yaml")
     val confPath = new Path(confFile.getAbsolutePath)
-    GlobalConfiguration.loadConfiguration(confDirPath)
-    yarnClient.setFlinkConfigurationObject(flinkConfiguration)
+    yarnClient.setFlinkConfiguration(flinkConfiguration)
     yarnClient.setConfigurationDirectory(confDirPath)
     yarnClient.setConfigurationFilePath(confPath)
 

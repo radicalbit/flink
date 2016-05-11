6c11e2a08a53b95b045354d3175a051810535576
chobeat <simone.robutti@gmail.com>
2016-05-11 17:14:25 +0200

    scala-checkstyle

--- flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/math/distributed/BlockMapper.scala
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/math/distributed/BlockMapper.scala
@@ -1,11 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.ml.math.distributed
 
 case class BlockMapper(//original matrix size
-                       numRows: Int, numCols: Int,
-                       //block size
-                       rowsPerBlock: Int, colsPerBlock: Int
-
-                        ) {
+                  val numRows: Int, val numCols: Int,
+                  //block size
+                  val rowsPerBlock: Int, val colsPerBlock: Int
+                   ) {
 
   val numBlockRows: Int = math.ceil(numRows * 1.0 / rowsPerBlock).toInt
   val numBlockCols: Int = math.ceil(numCols * 1.0 / colsPerBlock).toInt
@@ -13,12 +30,11 @@ case class BlockMapper(//original matrix size
 
   def getBlockIdByCoordinates(i: Int, j: Int): Int = {
 
-    if (i < 0 || j < 0||i >= numRows || j >= numCols)
+    if (i < 0 || j < 0 || i >= numRows || j >= numCols) {
       throw new IllegalArgumentException(s"Invalid coordinates ($i,$j).")
+    }
 
     else {
-
-
       val mappedRow = i / rowsPerBlock
       val mappedColumn = j / colsPerBlock
       val res = mappedRow * numBlockCols + mappedColumn
@@ -29,19 +45,23 @@ case class BlockMapper(//original matrix size
   }
 
   def getBlockMappedCoordinates(blockId: Int): (Int, Int) = {
-    if (blockId < 0||blockId > numBlockCols * numBlockRows)
-      throw new IllegalArgumentException(s"BlockId numeration starts from 0. $blockId is not a valid Id")
+    if (blockId < 0 || blockId > numBlockCols * numBlockRows) {
+      throw new IllegalArgumentException(
+        s"BlockId numeration starts from 0. $blockId is not a valid Id"
+      )
+    }
 
     else {
       val i = blockId / numBlockCols
       val j = blockId % numBlockCols
-     (i, j)
+      (i, j)
     }
   }
 
 
   /**
-   * Translates absolute coordinates to the mapped coordinates of the block these coordinates belong to
+   * Translates absolute coordinates to the mapped coordinates of the block
+   * these coordinates belong to.
    * @param i
    * @param j
    * @return
@@ -51,11 +71,13 @@ case class BlockMapper(//original matrix size
 
   def getBlockIdByMappedCoord(i: Int, j: Int): Int = {
 
-    if (i < 0 || j < 0 || i >= numBlockRows || j >= numBlockCols)
+    if (i < 0 || j < 0 || i >= numBlockRows || j >= numBlockCols) {
       throw new IllegalArgumentException(s"Invalid coordinates ($i,$j).")
+    }
 
-    else
+    else {
       i * numBlockCols + j
+    }
 
   }
 
--- flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/math/distributed/MatrixConversionsTest.scala
+++ flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/math/distributed/MatrixConversionsTest.scala
@@ -1,46 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.ml.math.distributed
 
 import org.apache.flink.api.scala._
-import org.apache.flink.ml.math.distributed.DistributedRowMatrix
-import org.scalatest.{Matchers, FlatSpec}
+import org.apache.flink.ml.math.SparseMatrix
+import org.scalatest.{FlatSpec, Matchers}
 
 class MatrixConversionsTest extends FlatSpec with Matchers {
 
-  "RowGroupReducer.calculateSlices" should "return correct slicing ranges" in{
-    val env=ExecutionEnvironment.getExecutionEnvironment
+  "RowGroupReducer.calculateSlices" should "return correct slicing ranges" in {
+    val env = ExecutionEnvironment.getExecutionEnvironment
 
 
-    val rowGroupReducer1=new RowGroupReducer(3,3,8,8)
+    val rowGroupReducer1 = new RowGroupReducer(3, 3, 8, 8)
 
-    rowGroupReducer1.calculateSlices() shouldBe Seq((0,2),(3,5),(6,7))
+    rowGroupReducer1.calculateSlices() shouldBe Seq((0, 2), (3, 5), (6, 7))
 
-    val rowGroupReducer2=new RowGroupReducer(25,25,1000,1000)
+    val rowGroupReducer2 = new RowGroupReducer(25, 25, 1000, 1000)
 
     rowGroupReducer2.calculateSlices().size shouldBe 40
 
-    val rowGroupReducer3=new RowGroupReducer(22,22,1000,1000)
+    val rowGroupReducer3 = new RowGroupReducer(22, 22, 1000, 1000)
 
-    rowGroupReducer3.calculateSlices().last shouldBe (990,999)
+    rowGroupReducer3.calculateSlices().last shouldBe(990, 999)
 
   }
   "DistributedRowMatrix.toBlockMatrix" should "preserve the matrix structure after conversion" in {
-    val env=ExecutionEnvironment.getExecutionEnvironment
-    val rawSampleData=List(
-      (0,0,3.0),
-      (0,1,1.0),
-      (0,3,4.0),
-      (2,3,60.0),
-      (1,2,50.0),
-      (1,1,12.0),
-      (2,1,14.0),
-      (3,2,18.0)
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val rawSampleData = List(
+      (0, 0, 3.0),
+      (0, 1, 1.0),
+      (0, 3, 4.0),
+      (2, 3, 60.0),
+      (1, 2, 50.0),
+      (1, 1, 12.0),
+      (2, 1, 14.0),
+      (3, 2, 18.0)
     )
 
 
-    val d1=DistributedRowMatrix.fromCOO(env.fromCollection(rawSampleData),4,4)
-    val blockMatrix=d1.toBlockMatrix(2,2)
-    blockMatrix.getDataset.collect().foreach(println)
+    val d1 = DistributedRowMatrix.fromCOO(env.fromCollection(rawSampleData), 4, 4)
+    val blockMatrix = d1.toBlockMatrix(2, 2).getDataset.collect
+
+    val block0 = Block.apply(SparseMatrix.fromCOO(2, 2, List(
+      (0, 0, 3.0),
+      (1, 0, 0.0),
+      (0, 1, 1.0),
+      (1, 1, 12.0)
+    )))
+
+    val block1 = Block.apply(SparseMatrix.fromCOO(2, 2, List(
+      (0, 0, 0.0),
+      (1, 0, 50.0),
+      (0, 1, 4.0),
+      (1, 1, 0.0)
+    )))
+
+    val block2 = Block.apply(SparseMatrix.fromCOO(2, 2, List(
+      (0, 0, 0.0),
+      (1, 0, 0.0),
+      (0, 1, 14.0),
+      (1, 1, 0.0)
+    )))
+
+    val block3 = Block.apply(SparseMatrix.fromCOO(2, 2, List(
+      (0, 0, 0.0),
+      (1, 0, 18.0),
+      (0, 1, 60.0),
+      (1, 1, 0.0)
+    )))
+
+    blockMatrix.toSet shouldBe Set((0, block0),
+      (1, block1),
+      (2, block2),
+      (3, block3))
+
+
+    val data2=(39,39,123.0)::(5,30,42.0)::rawSampleData
+    val d2 = DistributedRowMatrix.fromCOO(env.fromCollection(data2), 40, 40)
+    val blockMatrix2=d2.toBlockMatrix(3,20)
+    val dataMap=blockMatrix2.getDataset.collect().toMap
+
+    dataMap(3).getBlockData(2,10) shouldBe 42.0
+    dataMap.map(x=>(x._1,x._2.toBreeze)).foreach(println)
+
+    dataMap(blockMatrix2.getNumBlocks-1).getBlockData(0,19) shouldBe 123.0
+
+    d2.getNumCols shouldBe blockMatrix2.getNumCols
+    d2.getNumRows shouldBe blockMatrix2.getNumRows
 
   }
-
 }

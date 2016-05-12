7322054e7ea87aa6afa9e53936078fa2f34db7fa
chobeat <simone.robutti@gmail.com>
2016-05-12 12:17:57 +0200

    toRowMatrix (wo/ test)

--- flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/math/distributed/BlockMatrix.scala
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/math/distributed/BlockMatrix.scala
@@ -21,8 +21,8 @@ package org.apache.flink.ml.math.distributed
 
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.scala._
+import org.apache.flink.ml.math.SparseVector
 import org.apache.flink.ml.math.distributed.BlockMatrix.BlockID
-
 
 //TODO: test EVERYTHING
 class BlockMatrix(
@@ -49,7 +49,7 @@ class BlockMatrix(
     require(this.getColsPerBlock == other.getColsPerBlock)
     require(this.getRowsPerBlock == other.getRowsPerBlock)
 
-    val joinedBlocks = data.join(other.getDataset).where(x=>x._1).equalTo(x=>x._1)
+    val joinedBlocks = data.join(other.getDataset).where(x => x._1).equalTo(x => x._1)
     val multipliedJoinedBlocks = joinedBlocks.map(new MultiplyMapper(blockMapper))
 
     val reducedBlocks = multipliedJoinedBlocks.reduce(
@@ -73,10 +73,54 @@ class BlockMatrix(
     )
   }
 
+
+  def toRowMatrix: DistributedRowMatrix = {
+    val indexedRows = data
+    //map id to mapped coordinates
+    .map(
+      block=>(blockMapper.getBlockMappedCoordinates(block._1),block)
+    )
+      //group by block row
+    .groupBy(blockWithCoord=>blockWithCoord._1._1)
+      //turn a group of blocks in a seq of rows
+    .reduceGroup(blockGroup=>{
+
+    require(blockGroup.nonEmpty)
+
+    val groupRow=blockGroup.next()._1._1
+    //all blocks must have the same row
+    require(blockGroup.forall(block=>block._1._1==groupRow))
+
+    //map every block to its mapped column
+    val groupElements=blockGroup
+    .map(block=>(block._1._2,block._2._2) )
+    //sort by column
+    .toList.sortBy(_._1)
+    //unpack values
+    .flatMap(
+    block=>
+    block._2.getBlockData.toList
+    //map coordinates from block space to original space
+    .map(element=>{
+      val (i,j,value)=element
+
+          (i+(groupRow*getRowsPerBlock),j+block._1,value)
+    })
+    )
+    groupElements
+    .groupBy(_._1)
+    .map(row=>{
+    val cooVector=row._2.map(x=>(x._2,x._3))
+    IndexedRow(row._1,SparseVector.fromCOO(getNumCols,cooVector))}
+
+    )
+    }).flatMap(x => x)
+    new DistributedRowMatrix(indexedRows, Some(getNumRows), Some(getNumCols))
+  }
 }
 
-class MultiplyMapper (blockMapper:BlockMapper)
-  extends RichMapFunction[((BlockMatrix.BlockID, Block), (BlockMatrix.BlockID, Block)),(Int, Int, Block)]{
+class MultiplyMapper(blockMapper: BlockMapper)
+  extends RichMapFunction[((BlockMatrix.BlockID, Block), (BlockMatrix.BlockID, Block)), (Int, Int, Block)] {
 
   override def map(value: ((BlockMatrix.BlockID, Block), (BlockMatrix.BlockID, Block))): (Int, Int, Block) = {
     val ((idl, left), (idr, right)) = value

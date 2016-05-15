65ffaf74d40c1402956421d5b90ece3ea20f4ee8
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-05-15 10:04:15 +0200

    [hotfix] [release] Update change-version script

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="1.0-SNAPSHOT"
-NEW="1.0.3-rbp-001-SNAPSHOT"
+OLD="1.0.3-rbp-001-SNAPSHOT"
+NEW="1.0.3-rbp-001"
 
 HERE=` basename "$PWD"`
 if [[ "$HERE" != "tools" ]]; then

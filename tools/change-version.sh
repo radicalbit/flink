9d43f3c14a5fe73d729e2df241ef09ff7fdc300a
robbenti <roberto.bentivoglio@radicalbit.io>
2016-09-12 17:43:22 +0400

    Prepared release 1.1.2-rbd-001

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="1.0-SNAPSHOT"
-NEW="1.1-SNAPSHOT"
+OLD="1.1.2"
+NEW="1.1.2-rbd-001"
 
 
 HERE=` basename "$PWD"`
